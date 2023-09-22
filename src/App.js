9562acb7fe03a6c5abb3f830f40afb9c60785d88
Leonard Bernstein <lbernstein@yahoo.com>
2023-09-22 00:22:40 -0400

    Initial Checkin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function App() {
   const value = 'Worldd';
-  return <div>Hello {value}</div>;
+  //return <div>Hello {value}</div>;
+
+  const [data, setData] = React.useState('');
+
+  React.useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      const aaa = text.concat("ddd")
+      setData(aaa);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
